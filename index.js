1d0ded5b37f72dfc5a04e3e4c94a8fc702bf0998
Brian Shumate <brian@couchbase.com>
2015-11-22 13:28:08 -0500

    Initial toy project with flaws galore

---
+++ index.js
@@ -0,0 +1,183 @@
+#! /usr/bin/env node
+
+const async = require('async')
+const autoprefixer = require('metalsmith-autoprefixer')
+const branch = require('metalsmith-branch')
+const calc = require('postcss-calc')
+const chalk = require('chalk')
+const cleanCss = require('metalsmith-clean-css')
+const customMedia = require('postcss-custom-media')
+const customProperties = require('postcss-custom-properties')
+const duo = require('metalsmith-duo')
+const fingerprint = require('metalsmith-fingerprint')
+const fs = require('fs-extra')
+const GitHubApi = require('github')
+const github = new GitHubApi({
+  version: '3.0.0',
+  protocol: 'https'
+})
+const layouts = require('metalsmith-layouts')
+const metalsmith = require('metalsmith')
+const minify = require('metalsmith-html-minifier')
+const postcss = require('metalsmith-postcss')
+const ProgressBar = require('progress')
+const rename = require('metalsmith-rename')
+
+if (process.argv.length < 4) {
+  console.log(chalk.yellow('USAGE: nerdherder <GitHub API token> <location>'))
+  process.exit(1)
+}
+
+github.authenticate({
+  type: 'token',
+  token: process.argv[2]
+})
+
+var users = []
+var wantUsers = -1
+var pageNumber = 0
+
+const location = process.argv[3]
+
+async.series([
+  function (nextStep) {
+    async.whilst(
+      function () { return ((wantUsers < 0) || (users.length < wantUsers)) },
+      function (nextPage) {
+        github.search.users({
+          q: 'location:"' + location + '"',
+          page: pageNumber,
+          per_page: 100
+        }, function (err, result) {
+          if (err) {
+            return nextStep(err)
+          }
+          if (result.items) {
+            users = users.concat(result.items)
+          }
+          if ((wantUsers < 0) && (result.total_count)) {
+            wantUsers = result.total_count
+            const locationHeader = '<h2 class="text-right">Herding ' + wantUsers + ' nerds in ' + location + '</h2>'
+            const locationTemplatePath = 'partials/location.hbs'
+            fs.writeFile(locationTemplatePath, locationHeader, function (err) {
+              if (err) throw err
+            })
+          }
+          // console.log(chalk.white('INFO: got page ' + pageNumber))
+          pageNumber += 1
+          return nextPage(null)
+        })
+      },
+      nextStep
+    )
+  },
+  function (nextStep) {
+    const nerdsTemplatePath = 'partials/allnerds.hbs'
+    fs.remove(nerdsTemplatePath, function (err) {
+      if (err) return console.error(chalk.red('ERROR: ' + err))
+    })
+    // console.log(chalk.white('Number of users: ' + users.length))
+    var bar = new ProgressBar('Herding nerds [:bar] :percent', {
+      complete: '=',
+      incomplete: ' ',
+      width: 40,
+      total: users.length
+    })
+    async.eachSeries(users, function (user, nextUser) {
+      github.user.getFrom({
+        user: user.login,
+        sort: 'updated',
+        direction: 'desc',
+        page: 0,
+        per_page: 100
+      }, function (err, result) {
+        if (err) {
+          throw err
+        }
+        if (result) {
+          var fullName = result.name
+          var avatarURL = result.avatar_url
+          var blogURL = '#'
+          var canHire = 'No'
+          var companyName = 'None'
+          var emailAddress = '#'
+          var emailButton = 'disabled hollow button'
+          var followerCount = result.followers
+          var ghUser = result.login
+          var repoCount = result.public_repos
+          var webButton = 'disabled hollow button'
+          if (result.blog) {
+            var prefix = 'http://'
+            if (result.blog.substr(0, prefix.length) !== prefix) {
+              blogURL = prefix + result.blog
+            }
+            webButton = 'secondary hollow button'
+          }
+          if (result.company) { companyName = result.company }
+          if (result.email) {
+            emailAddress = result.email
+            emailButton = 'hollow button'
+          }
+          if (result.hireable === true) { canHire = 'Yes' }
+          var nerd = ''
+          nerd = '{{> nerd gh_user="' + ghUser + '" name="' + fullName + '" avatar="' + avatarURL + '" company="' + companyName + '" for_hire="' + canHire + '" repo_count="' + repoCount + '" follower_count="' + followerCount + '" email_url="' + emailAddress + '" website_url="' + blogURL + '" email_button_class="' + emailButton + '" web_button_class="' + webButton + '" }}\n'
+          fs.appendFile(nerdsTemplatePath, nerd, function (err) {
+            if (err) throw err
+            // console.log(chalk.white('INFO: Writing user ' + ghUser))
+            bar.tick()
+          })
+        }
+        return nextUser(null)
+      })
+    }, nextStep)
+  },
+  function (nextStep) {
+    const plugins = [
+      customMedia,
+      customProperties,
+      calc
+    ]
+    const supported = {browsers: ['> 1%', 'last 2 versions', 'IE >= 9']}
+    metalsmith(__dirname)
+      .use(branch('*.hbs')
+        .use(layouts({
+          engine: 'handlebars',
+          partials: 'partials'
+        })))
+      // process CSS
+      .use(duo({entry: ['styles/style.css']}))
+      .use(autoprefixer(supported))
+      .use(postcss(plugins))
+      .use(cleanCss({files: 'styles/style.css'}))
+      .use(fingerprint({pattern: ['styles/style.css']}))
+      // process templates
+      .use(rename([[/\.hbs$/, '.html']]))
+      .use(minify())
+      // build
+      .build(function (err) {
+        if (err) { throw err }
+      })
+    setTimeout(function () {
+      console.log(chalk.green('SUCCESS: Generated static site in directory: ./build'))
+      const web = require('node-static')
+      const port = 8080
+      const http = require('http')
+      const file = new web.Server('./build', {
+        cache: 3600,
+        gzip: true
+      })
+      console.log(chalk.green('SUCCESS: Nerds herded at http://127.0.0.1:8080'))
+      http.createServer(function (request, response) {
+        request.addListener('end', function () {
+          file.serve(request, response)
+        }).resume()
+      }).listen(port)
+      nextStep
+    }, Math.floor(Math.random() * 1000))
+  }
+], function (err) {
+  if (err) {
+    throw err
+  }
+  process.exit(0)
+})
